abb54a3f1c5596f378460182c952bd79397ed25e
Akmal Sindarov <asauz777@gmail.com>
2024-09-28 12:41:20 +1000

    Update main.go

--- app/main.go
+++ app/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	_ "github.com/jackc/pgx/v4/stdlib" // Use pgx driver for PostgreSQL
 	"github.com/joho/godotenv"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -35,5 +37,27 @@ func main() {
 		log.Fatal("Cannot connect to DB:", err)
 	} else {
 		fmt.Println("Connected to the database successfully! 🎉")
+	}
+
+	// Set up Gin router ---------------------------------------------------------
+	r := gin.New() // Creates a new Gin engine without default middleware
+
+	r.Use(gin.Logger())   // Use Logger middleware
+	r.Use(gin.Recovery()) // Use Recovery middleware
+
+	// Serve static files
+	r.Static("/css", "./static/css")
+	r.Static("/js", "./static/js")
+
+	// Render HTML template
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	// Start the server
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal("Failed to run the server:", err)
 	}
 }
